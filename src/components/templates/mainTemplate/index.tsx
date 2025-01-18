e2f23c095553a3088bc478910ba471599f05fe90
gitFeli <feli.esunco@gmail.com>
2025-01-18 13:19:55 +0330

    change logo

--- src/components/templates/mainTemplate/index.tsx
+++ src/components/templates/mainTemplate/index.tsx
@@ -1,5 +1,5 @@
 import { ArrowRight2 } from "iconsax-react";
-import logo from "../../../assets/images/Group 48095662.svg";
+import logo from "../../../assets/images/logo.svg";
 import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -15,7 +15,11 @@ const MainTemplate = (props: IProps) => {
   return (
     <div className="flex flex-col h-screen">
       <div className="bg-primaries-100 rounded-b-2xl items-center pt-8 flex flex-col h-52">
-        <ArrowRight2 size={24} className="absolute text-white right-4" onClick={() => navigate(-1)}/>
+        <ArrowRight2
+          size={24}
+          className="absolute text-white right-4"
+          onClick={() => navigate(-1)}
+        />
         <img src={logo} alt="" className="w-[90px]" />
       </div>
 
